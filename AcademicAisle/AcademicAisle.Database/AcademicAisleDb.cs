b0178ae3c53b7839069971137e89433d994d5fc4
manish353730 <manishk353730@gmail.com>
2024-03-05 23:57:56 +0530

    new Mvc

---
+++ AcademicAisle/AcademicAisle.Database/AcademicAisleDb.cs
@@ -0,0 +1,20 @@
+﻿using AcademicAsile.entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicAisle.Database
+{
+    public class AcademicAisleDb: DbContext
+    {
+        public AcademicAisleDb(): base("AisleConnection")
+        { 
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> MyProducts { get; set; }
+    }
+}
